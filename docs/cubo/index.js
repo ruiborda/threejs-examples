ff1a7510583128d87d1b97efe2b805e013c4ec69
ruiborda <bordarui@gmail.com>
2021-08-21 20:33:17 -0500

    cubo

--- docs/cubo/index.js
+++ docs/cubo/index.js
@@ -1,23 +1,40 @@
-import {Scene,Color,PerspectiveCamera, WebGLRenderer,BoxGeometry,MeshBasicMaterial,Mesh} from "../snowpack/pkg/three.js";
+import {
+    Scene,
+    Color,
+    PerspectiveCamera,
+    WebGLRenderer,
+    BoxGeometry,
+    MeshBasicMaterial,
+    Mesh,
+    Fog,
+    TextureLoader
+} from "../snowpack/pkg/three.js";
+import fondourl from './fondo.jpg.proxy.js'
+
 const scene = new Scene();
+scene.background = new Color("skyblue");
+scene.fog = new Fog(0x76456c, 0, 8);
 const renderer = new WebGLRenderer();
 const camera = new PerspectiveCamera(
     75,
-    window.innerWidth/window.innerHeight
+    window.innerWidth / window.innerHeight
 );
-const geometry = new BoxGeometry();
-const material = new MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-const cube = new Mesh(geometry, material);
-
-scene.background = new Color(0x2a3b4c);
+const loader = new TextureLoader()
+loader.load(fondourl, function (texture) {
+    scene.background=texture
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial({color: 0x00ff00});
+const cube = new Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 7;
+camera.position.z = 3;
 
-var animate = function(){
-    requestAnimationFrame(animate);
+var animate = function () {
+    window.requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
